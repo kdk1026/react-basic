658a20ad727720e3373b92527e03f731093a21d0
kdk1026 <kdk1026@gmail.com>
2024-06-10 12:38:28 +0900

    route 분리

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
-import Counter from "./pages/Counter";
-import Input from "./pages/Input";
-import Input2 from "./pages/Input2";
-import UserList from "./pages/UserList";
-import Movies from "./pages/Movies";
-import Param1 from "./pages/Param1";
-import Param2 from "./pages/Param2";
-import Param3 from "./pages/Param3";
-import LinkSendData from "./pages/LinkSendData";
-import LinkReceiveData from "./pages/LinkReceiveData";
-import Main from "./pages/Main";
-import Articles from "./pages/article/Articles";
-import Article from "./pages/article/Article";
-import Focus from "./pages/Focus";
-import TextArea from "./pages/TextArea";
-import CheckBox from "./pages/CheckBox";
-import Radio from "./pages/Radio";
-import Select from "./pages/Select";
-import InputFile from "./pages/InputFile";
-import ReactSelect from "./pages/ReactSelect";
-import ReactCookie from "./pages/ReactCookie";
-import RouterHistory from "./pages/RouterHistory";
-import RouterHistoryMove from "./pages/RouterHistoryMove";
-import FileDownload from "./pages/FileDownload";
-import FileUpload from "./pages/FileUpload";
+import CommonRoute from "./components/CommonRoute";
 
 function App() {
   return (
@@ -46,36 +20,7 @@ function App() {
           <Link to="/react-cookie">react-cookie</Link> | <Link to="/routerHistory">RouterHistory</Link> | &nbsp;
           <Link to="/fileDownload">파일 다운로드</Link> | <Link to="/fileUpload">파일 업로드</Link>
         </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/input" element={<Input />} />
-          <Route path="/input2" element={<Input2 />} />
-          <Route path="/userList" element={<UserList />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/param/:id" element={<Param1 />} />
-          <Route path="/param" element={<Param2 />} />
-          <Route path="/param2" element={<Param3 />} />
-          <Route path="/linkSendData" element={<LinkSendData />} />
-          <Route path="/linkReceiveData" element={<LinkReceiveData />} />
-          <Route path="/main" element={<Main />}>
-            <Route path="article" element={<Articles />} />
-            <Route path="article/:id" element={<Article />} />
-          </Route>
-          <Route path="/focus" element={<Focus />} />
-          <Route path="/textarea" element={<TextArea />} />
-          <Route path="/checkbox" element={<CheckBox />} />
-          <Route path="/radio" element={<Radio />} />
-          <Route path="/select" element={<Select />} />
-          <Route path="/inputFile" element={<InputFile />} />
-          <Route path="/react-select" element={<ReactSelect />} />
-          <Route path="/react-cookie" element={<ReactCookie />} />
-          <Route path="/routerHistory" element={<RouterHistory />} />
-          <Route path="/routerHistoryMove" element={<RouterHistoryMove />} />
-          <Route path="/fileDownload" element={<FileDownload />} />
-          <Route path="/fileUpload" element={<FileUpload />} />
-        </Routes>
+        <CommonRoute />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/CommonRoute.js
@@ -0,0 +1,68 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Counter from "../pages/Counter";
+import Input from "../pages/Input";
+import Input2 from "../pages/Input2";
+import UserList from "../pages/UserList";
+import Movies from "../pages/Movies";
+import Param1 from "../pages/Param1";
+import Param2 from "../pages/Param2";
+import Param3 from "../pages/Param3";
+import LinkSendData from "../pages/LinkSendData";
+import LinkReceiveData from "../pages/LinkReceiveData";
+import Main from "../pages/Main";
+import Articles from "../pages/article/Articles";
+import Article from "../pages/article/Article";
+import Focus from "../pages/Focus";
+import TextArea from "../pages/TextArea";
+import CheckBox from "../pages/CheckBox";
+import Radio from "../pages/Radio";
+import Select from "../pages/Select";
+import InputFile from "../pages/InputFile";
+import ReactSelect from "../pages/ReactSelect";
+import ReactCookie from "../pages/ReactCookie";
+import RouterHistory from "../pages/RouterHistory";
+import RouterHistoryMove from "../pages/RouterHistoryMove";
+import FileDownload from "../pages/FileDownload";
+import FileUpload from "../pages/FileUpload";
+
+function CommonRoute() {
+    return (
+        <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/input" element={<Input />} />
+          <Route path="/input2" element={<Input2 />} />
+          <Route path="/userList" element={<UserList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/param/:id" element={<Param1 />} />
+          <Route path="/param" element={<Param2 />} />
+          <Route path="/param2" element={<Param3 />} />
+          <Route path="/linkSendData" element={<LinkSendData />} />
+          <Route path="/linkReceiveData" element={<LinkReceiveData />} />
+          <Route path="/main" element={<Main />}>
+            <Route path="article" element={<Articles />} />
+            <Route path="article/:id" element={<Article />} />
+          </Route>
+          <Route path="/focus" element={<Focus />} />
+          <Route path="/textarea" element={<TextArea />} />
+          <Route path="/checkbox" element={<CheckBox />} />
+          <Route path="/radio" element={<Radio />} />
+          <Route path="/select" element={<Select />} />
+          <Route path="/inputFile" element={<InputFile />} />
+          <Route path="/react-select" element={<ReactSelect />} />
+          <Route path="/react-cookie" element={<ReactCookie />} />
+          <Route path="/routerHistory" element={<RouterHistory />} />
+          <Route path="/routerHistoryMove" element={<RouterHistoryMove />} />
+          <Route path="/fileDownload" element={<FileDownload />} />
+          <Route path="/fileUpload" element={<FileUpload />} />
+        </Routes>
+        </>
+    )
+}
+
+export default CommonRoute;
