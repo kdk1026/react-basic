75efcb6bc4f239fadb871649913f9968101e242e
kdk1026 <kdk1026@gmail.com>
2025-02-21 20:00:21 +0900

    추가

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -7,9 +7,24 @@ export const setCookie = (name, value, options) => {
 };
 
 export const getCookie = (name) => {
+    // Array 혹은 OBject면 JSON.parse(name) 자동으로 해줌
     return cookies.get(name);
 }
 
 export const removeCookie = (name, options) => {
     return cookies.remove(name, {...options});
+}
+
+export const setArrayInCookie = (name, array, options) => {
+    if ( !isEmptyArray(array) ) {
+        //JSON.stringify(array) 자동으로 해줌
+        setCookie(name, array, {...options});
+    }
+};
+
+export const setObjectInCookie = (name, object, options) => {
+    if ( !isEmptyObject(object) ) {
+        //JSON.stringify(object) 자동으로 해줌
+        setCookie(name, object, {...options});
+    }
 }
